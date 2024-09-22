d4109cf8bac7085263be76de3cbeeb03bac13873
YisuHou1 <yisu.hou@mail.utoronto.ca>
2024-09-22 01:22:21 +0000

    updated code for raw data download

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -10,22 +10,55 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
+library(dplyr)
 
 
-#### Download data ####
+# police budget data
 # get package
-package <- show_package("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+package_bud <- show_package("668434ee-9541-40a8-adb6-0ad805fcc9b6")
 
 # get all resources for this package
-resources <- list_package_resources("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+resources_bud <- list_package_resources("668434ee-9541-40a8-adb6-0ad805fcc9b6")
 
-# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
-datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+# identify datastore resources; by default, Toronto Open Data sets datastore 
+# resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources_bud <- 
+  filter(resources_bud, tolower(format) %in% c('csv', 'geojson'))
 
-# load the first datastore resource as a sample
-data <- filter(datastore_resources, row_number()==1) %>% 
-  get_resource()
+# load datasets for 2020 to 2024
+data_bud0 <- filter(datastore_resources_bud, row_number()==1) %>% get_resource()
+data_bud1 <- filter(datastore_resources_bud, row_number()==2) %>% get_resource()
+data_bud2 <- filter(datastore_resources_bud, row_number()==7) %>% get_resource()
+data_bud3 <- filter(datastore_resources_bud, row_number()==5) %>% get_resource()
+data_bud4 <- filter(datastore_resources_bud, row_number()==6) %>% get_resource()
 
+# edit column names of older datasets for binding consistency
+names(data_bud0)[names(data_bud0) == '_id'] <- 'X_id'
+names(data_bud1)[names(data_bud1) == '_id'] <- 'X_id'
+
+# combine yearly datasets into aggregate raw dataset, store as csv
+raw_budget_data <- rbind(data_bud0, data_bud1, data_bud2, data_bud3,
+                         data_bud4)
+write_csv(raw_budget_data, "data/raw_data/raw_budget_data.csv") 
+
+
+# now retrieve reported crimes dataset
+# get package
+package_cri <- show_package("police-annual-statistical-report-reported-crimes")
+
+# get all resources for this package
+resources_cri <- 
+  list_package_resources("police-annual-statistical-report-reported-crimes")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore 
+# resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources_cri <- 
+  filter(resources_cri, tolower(format) %in% c('csv', 'geojson'))
+
+# load the crime dataset
+data_cri <- filter(datastore_resources_cri, row_number()==2) %>% get_resource()
 
 #### Save data ####
-write_csv(data, "data/raw_data/raw_data.csv") 
+write_csv(data_cri, "data/raw_data/raw_crime_data.csv") 
+
+
