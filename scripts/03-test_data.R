2c75c4dd1e3edc2e697ebde757256e28fdf4ab2e
YisuHou1 <yisu.hou@mail.utoronto.ca>
2024-09-22 01:32:12 +0000

    updated tests for simulated and raw data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -14,14 +14,22 @@ library(tidyverse)
 #### Test data ####
 data_bud <- read_csv("data/raw_data/simulated_budget.csv")
 data_cri <- read_csv("data/raw_data/simulated_crime.csv")
+raw_bud <- read_csv("data/raw_data/raw_budget_data.csv")
+raw_cri <- read_csv("data/raw_data/raw_crime_data.csv")
 
 # Test for negative numbers
 data_bud$budget |> min() <= 0
 data_cri$crime |> min() <= 0
+raw_bud$Amount |> min() <= 0
+raw_cri$COUNT_ |> min() <= 0
+# the raw dataset for budget contains negative numbers because 
+# the police department spends money
 
 # Test for NAs
 all(is.na(data_bud$budget))
 all(is.na(data_cri$crime))
+all(is.na(raw_bud$Amount))
+all(is.na(raw_cri$COUNT_))
 
 
 
